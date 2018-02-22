0a48571e17183a65e6e752828064c16bcc57261c
John Leonardo <jdleonardo5@icloud.com>
2018-02-22 23:52:55 +0000

    screwed up, have to fix on desktop

--- Cheshire/cheshire.py
+++ Cheshire/cheshire.py
@@ -3,6 +3,32 @@ import asyncio
 
 client = discord.Client()
 
+greetingQuotes = [
+	"Hello, sunshine!"
+	"Howdy, partner!"
+	"Hey, howdy, hi!"
+	"Whats kickin, little chicken?"
+	"Peek-a-boo!"
+	"Howdy-doody!"
+	"Hey there, freshman!"
+	"My name's Cheshire, and I'm a bad guy."
+	"Hi, mister!"
+	"I come in peace!"
+	"Put that cookie down!"
+	"Ahoy, matey!"
+	"Hiya!"
+	"Ello, gov'nor!"
+	"Top of the mornin' to ya!"
+	"Whats crackin?"
+	"GOOOOOD MORNING, VIETNAM!"
+	"Sup, homeslice?"
+	"This call may be recorded for training purposes."
+	"Howdy, howdy, howdy!"
+	"Im Batman"
+	"At least, we meet for the first time for the last time!",
+]
+
+
 @client.event
 async def on_ready():
 	print('Logged in as')
