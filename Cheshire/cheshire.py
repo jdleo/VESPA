13dbcbfb89ad80eb315766338b833b801db902ae
John Leonardo <jdleonardo5@icloud.com>
2018-02-22 16:00:32 -0800

    updated greetings

--- Cheshire/cheshire.py
+++ Cheshire/cheshire.py
@@ -3,29 +3,49 @@ import asyncio
 
 client = discord.Client()
 
-greetingQuotes = [
-	"Hello, sunshine!"
-	"Howdy, partner!"
-	"Hey, howdy, hi!"
-	"Whats kickin, little chicken?"
-	"Peek-a-boo!"
-	"Howdy-doody!"
-	"Hey there, freshman!"
-	"My name's Cheshire, and I'm a bad guy."
-	"Hi, mister!"
-	"I come in peace!"
-	"Put that cookie down!"
-	"Ahoy, matey!"
-	"Hiya!"
-	"Ello, gov'nor!"
-	"Top of the mornin' to ya!"
-	"Whats crackin?"
-	"GOOOOOD MORNING, VIETNAM!"
-	"Sup, homeslice?"
-	"This call may be recorded for training purposes."
-	"Howdy, howdy, howdy!"
-	"Im Batman"
+greetings = [
+	"Aloha",
+	"Hola",
+	"Que pasa?",
+	"Bonjour",
+	"Hallo",
+	"Ciao",
+	"Konnichiwa",
+	"Ello, mate",
+	"Heeey, baaaaaby",
+	"Oh, yoooouhoooo!",
+	"How you doin?",
+	"Why, hello there!",
+	"Hello, sunshine!",
+	"Howdy, partner!",
+	"Hey, howdy, hi!",
+	"Whats kickin, little chicken?",
+	"Peek-a-boo!",
+	"Howdy-doody!",
+	"Hey there, freshman!",
+	"My name's Ralph, and I'm a bad guy.",
+	"Hi, mister!",
+	"I come in peace!",
+	"Put that cookie down!",
+	"Ahoy, matey!",
+	"Hiya!",
+	"Ello, gov'nor!",
+	"Top of the mornin to ya!",
+	"Whats crackin?",
+	"GOOOOOD MORNING, VIETNAM!",
+	"Sup, homeslice?",
+	"This call may be recorded for training purposes.",
+	"Howdy, howdy, howdy!",
+	"I'm Batman.",
 	"At least, we meet for the first time for the last time!",
+	"Hello, who's there, I'm talking.",
+	"Here's Johnny!",
+	"You know who this is.",
+	"Ghostbusters, whatya want?",
+	"Yo!",
+	"Whaddup",
+	"Greetings and saluations!",
+	"Doctor."
 ]
 
 
